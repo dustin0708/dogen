bda68364e698893a51eb4e018d7083fa645f39e8
yubeigen <yubeigen0708@163.com>
2019-09-03 22:36:48 +0800

    add crontab task

---
+++ celery/crontab/cron_stock.py
@@ -0,0 +1,29 @@
+#-*-coding:utf-8-*-
+
+import sys
+import dogen
+import pymongo
+import traceback
+
+from celery import Celery
+from celery.task.schedules import crontab 
+from celery.decorators import periodic_task
+
+
+@periodic_task(run_every=crontab(day_of_week='1-6', hour='01'))
+def download_kdata():
+    """ 周期性执行从网络侧更新股票数据的任务
+    """
+    try:
+        db = dogen.DbMongo()
+    except Exception:
+        traceback.print_exc()
+        return None
+        
+    print("periodic taks")
+
+    return None
+
+if __name__ == "__main__":
+    print("Welcome to " +  sys.argv[0] + " package.") 
+    
