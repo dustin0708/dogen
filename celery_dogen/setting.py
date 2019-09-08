014232b0f7e235693133fa7a1f60b1ab09ae30b3
yubeigen <yubeigen0708@163.com>
2019-09-09 00:24:58 +0800

    optimize

--- celery_dogen/setting.py
+++ celery_dogen/setting.py
@@ -25,13 +25,13 @@ beat_schedule = {
     ### 交易数据更新
     'exec-kdata-daily-pull': {
         'task': 'celery_dogen.tasks_of_kdata.dispatcher_of_daily_pull_update_kdata',
-        'schedule': crontab(day_of_week='1-6', hour='0', minute='01')
+        'schedule': crontab(day_of_week='2-6', hour='0', minute='01')
     },
 
     ### 涨停回调策略
     'exec-policy-hl-fallback': {
         'task': 'celery_dogen.tasks_of_policy.dispatcher_of_hl_fallback_match',
-        'schedule': crontab(day_of_week='1-6', hour='0', minute='21'),
+        'schedule': crontab(day_of_week='2-6', hour='0', minute='21'),
         'args': (None, None, None, True, 1000)
     },
 }
