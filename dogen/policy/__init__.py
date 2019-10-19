a5907abe34af79a56fdb79c029cbfff944bcddb3
dustin0708 <yubeigen0708@163.com>
2019-10-19 18:13:55 +0800

    optimize

--- dogen/policy/__init__.py
+++ dogen/policy/__init__.py
@@ -1,3 +1,3 @@
 #-*-coding:utf-8-*-
 
-__all__ = ['hl_fallback', 'hl_risekeep', 'rebound', 'uptrend', 'new_ipo']
+__all__ = ['hl_fallback', 'hl_risekeep', 'new_ipo', 'rebound', 'rebound_w', 'rs_fallback', 'uptrend']
