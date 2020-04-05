784dfb8899cca741c289b7ff8c2bc392f8c7b17e
dustin0708 <yubeigen0708@163.com>
2020-04-06 00:43:45 +0800

    optimize

--- dogen/stock/concept.py
+++ dogen/stock/concept.py
@@ -38,7 +38,7 @@ def parse_thsgn_file(filename):
 
         code = tds[0].text.split('.')[0]
         name = tds[1].text
-        cnpt = tds[4].text.strip().split(';-')
+        cnpt = tds[4].text.strip().split(';')
         indt = tds[7].text.split('-')[0]
 
         code = {CODE: code, NAME: name, CONCEPT: filter_from_black_list(cnpt), INDUSTRY: indt}
