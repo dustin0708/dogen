64932001e5d6b4d5b68c5e93e067aabb4bb3388e
dustin0708 <yubeigen0708@163.com>
2020-05-24 00:08:44 +0800

    opt

--- dogen/policy/up_fallback.py
+++ dogen/policy/up_fallback.py
@@ -96,17 +96,6 @@ def exclude_analyze(basic, kdata, pick_index, take_index, rise_range, policy_arg
         return True
 
     ### 特征四
-    if (from_index-high_index) < (high_index-take_index):
-        logger.debug("Invalid rise/fall range trades")
-        return True
-    tdata = kdata[pick_index:high_index]
-    rise_trade = tdata[tdata[dogen.R_CLOSE] > 0].index.size
-    fall_trade = tdata[tdata[dogen.R_CLOSE] < 0].index.size
-    if fall_trade < rise_trade*2:
-        logger.debug("Invalid fall-range")
-        return True
-
-    ### 特征五
     heap_lhigh = 0
     for temp_index in range(high_index, from_index):
         if kdata.iloc[temp_index][dogen.P_CLOSE] >= kdata.iloc[temp_index][dogen.L_HIGH]:
@@ -281,10 +270,7 @@ def match(codes, start=None, end=None, save_result=False, policy_args=None):
 
         >>> 排它条件
             三 股价市值在outstanding(100亿)和maxi_close(50以下)限制范围内
-            四 区间校验:
-                1) 上涨区间长于下跌区间
-                2) 下跌区间阴盛阳衰
-            五 涨停检查
+            四 涨停检查
                 1) 上涨区间排除连板
                 2) 三个月内有涨停
 
